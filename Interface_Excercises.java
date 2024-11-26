8810eb03f995bd354d14163a14c85cf758113f95
jayshreevinoth <45241188+jayshreevinoth@users.noreply.github.com>
2024-11-26 11:42:46 +0000

    Add files via upload

---
+++ Interface_Excercises.java
@@ -0,0 +1,26 @@
+interface firstInterface {
+    void printFirst();
+}
+
+interface secondInterface {
+    void printSecond();
+}
+
+class Demo implements firstInterface, secondInterface {
+
+    public void printFirst() {
+        System.out.println("first");
+    }
+
+    public void printSecond() {
+        System.out.println("second");
+    }
+}
+
+public class Interface_Excercises {
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        obj.printFirst();
+        obj.printSecond();
+    }
+}
